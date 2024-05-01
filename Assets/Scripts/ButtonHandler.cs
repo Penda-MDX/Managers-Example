3c4c6b4fc4265673c33eae887e5f64cd951fd6c8
Penda-MDX <p.tomlinson@mdx.ac.uk>
2024-05-01 11:58:06 +0100

    Initial commit

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TextMesh buttonText;
+    public Button buttonObj;
+
+    public string numberText;
+    public int symbolNumber;
+    public string symbolText;
+
+    public Color otherColor;
+    public Color startColor;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
